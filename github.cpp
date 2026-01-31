d32915224e854f185e1032798a8a88d526db46a3
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-01 00:36:56 +0200

    first stip to NZ

---
+++ github.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int operatingCost =16000000;
+
+    cout << "Apple inc Net Balance $ " <<1500000+650000+operatingCost; 
+    cout << "\n Tesla Cloud Service Net Balance $ "<<600000+50000+operatingCost;
+    operatingCost=400;
+    cout <<"\n SpaceX infrastructure Net Balance $ "<<1300000+70000+operatingCost;
+return 0;
+}
+
+        /*Please refactor the code and provide a detailed explanation 
+        of the modifications within the comments to enhance my technical learning*/
