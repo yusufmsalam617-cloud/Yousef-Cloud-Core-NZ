82905d3ed1b75d9ad8d663ccdd817ca8bcaa1197
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-01 23:29:31 +0200

    endl betar than \n

--- github.cpp
+++ github.cpp
@@ -4,12 +4,12 @@ int main()
 {
 long long operatingCost =16000000;
 
-    cout << "Apple inc Net Balance $ " <<1500000+650000+operatingCost; 
-    cout << "\nTesla Cloud Service Net Balance $ "<<600000+50000+operatingCost;
+    cout << endl << "Apple inc Net Balance $ " <<1500000+650000+operatingCost; 
+    cout << endl << "Tesla Cloud Service Net Balance $ "<<600000+50000+operatingCost;
     operatingCost=400;
-    cout <<"\nSpaceX infrastructure Net Balance $ "<<1300000+70000+operatingCost;
+    cout << endl << "SpaceX infrastructure Net Balance $ "<<1300000+70000+operatingCost;
 return 0;
 }
 
         /*Please refactor the code and provide a detailed explanation 
-        of the modifications within the comments to enhance my technical learning*/
+        of the modifications within the comments to enhance my technical learning*/ 
