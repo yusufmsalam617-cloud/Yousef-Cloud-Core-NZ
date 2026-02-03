cf98421b91a6d720e5d65e24aba97bdbc3ab56d5
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-03 22:35:53 +0200

    Update github.cpp
    Abdate

--- github.cpp
+++ github.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-using namespace std;
+
+using namespace std ;
 int main()
 {
-long long operatingCost =16000000;
-
-    cout << endl << "Apple inc Net Balance $ " <<1500000+650000+operatingCost; 
-    cout << endl << "Tesla Cloud Service Net Balance $ "<<600000+50000+operatingCost;
-    operatingCost=400;
-    cout << endl << "SpaceX infrastructure Net Balance $ "<<1300000+70000+operatingCost;
+long long operatingCost =1600000;
+cout<< "Apple inc Net Balance $ " <<1500000+650000+operatingCost<< endl ;
+cout<< "Tesla Cloud Service Net Balance $ " <<600000+50000+operatingCost<< endl;
+operatingCost=400;
+cout<< "SpaceX infrastructure Net Balance $ " <<1300000+70000+operatingCost<< endl;
 return 0;
 }
 
