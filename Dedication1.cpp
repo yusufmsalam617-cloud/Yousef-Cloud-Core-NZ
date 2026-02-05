58d3a01bd57cfa0c5e1952d5f3fd413c1c684c8b
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-06 00:19:59 +0200

    new step

---
+++ Dedication1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+string name ;
+int age;
+double balance;
+cout<<"enter your name";
+cin>>name;
+cout<<"inter your age";
+cin>>age;
+cout<<"enter your money";
+cin>>balance;
+cout<<"name is" <<std::endl;
+cout<<"age is" <<std::endl;
+cout<<"money is";
+cout<<"balance in NZ "<<balance*1.5 <<std::endl;
+return 0;
+}
+
---
+++ Dedication3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+string name ;
+int age;
+double balance;
+cout<<"enter your name";
+cin>>name;
+cout<<"inter your age";
+cin>>age;
+cout<<"enter your money";
+cin>>balance;
+cout<<"name is" <<name <<endl;
+cout<<"age is" <<age <<endl;
+cout<<"money is" <<balance <<endl;
+cout<<"balance in NZ "<<balance*1.5 <<endl;
+return 0;
+}
+
