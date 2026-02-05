366e2c1b12a619a6fcdaad366f45e2540c2c5950
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-05 03:37:47 +0200

    new update

---
+++ github3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+long long operatingCost =16000000;
+
+
+std::cout<< "Apple inc Net Balance $ "<<1500000+650000+operatingCost<<std::endl;
+std::cout<<"Tesla Cloud Service Net Balance $ "<<600000+50000+operatingCost<<std::endl;
+operatingCost=400;
+std::cout<<"SpaceX infrastructure Net Balance $ "<<1300000+70000+operatingCost<<std::endl;
+return 0;
+}
+
+        /*Please refactor the code and provide a detailed explanation 
+        of the modifications within the comments to enhance my technical learning*/ 
