c0b623bf49559fbe1a7bba09459c86f5a1230a32
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-05 03:59:22 +0200

    new step

---
+++ newstip.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,c;
+a=10;
+b=20;
+ c=30 ;
+cout << a+b+c <<std::endl;
+int d,e,f ;
+d=15;
+ e=20;
+ f=25;
+cout << d+e-f <<std::endl;
+return 0 ; 
+}
