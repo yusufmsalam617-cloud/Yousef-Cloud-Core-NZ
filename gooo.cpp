7378a55cc3e45ebe317928a58f713fda5a5505da
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-11 23:43:22 +0200

    replay

---
+++ gooo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string companyname;
+cout<<"please enter your name company";
+getline(cin , companyname);
+long long startupcapital;
+cout<<"enter your startupcapetal:";
+cin>>startupcapital;
+double montheexpenses;
+cout<<"enter your montheexpenses";
+cin>>montheexpenses;
+double remainignbalance;
+remainignbalance= startupcapital -(montheexpenses*12);
+cout<<"after one year your remainingbalancein NZ is:"<< remainignbalance*1.2 <<endl;
+return 0;
