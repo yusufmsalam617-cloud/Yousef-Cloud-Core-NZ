0d97b5b50f82bf7d457aa0dd61df79b7c46d93da
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-12 21:28:45 +0200

    to my dream

---
+++ dream1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int gigabayts;
+cout<<"how much gigabytes you need";
+cin>>gigabayts;
+int plan;
+cout<<"Basic Plan (2$ per GB)"<<endl;
+cout<<"Premium Plan (5$ per GB)"<<endl;
+cout<<" chois your plan";
+cin>>plan;
+double totalPrice;
+if(plan == 1) {totalPrice= gigabayts*2;}
+if(plan == 2) {totalPrice=gigabayts*5;}
+cout<<"The Total Price for your cloud plan is:";
+return 0;
+}
