12c35f42a4def0bef17f4b186b9369f4fbd52612
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-17 01:47:31 +0200

    loop

---
+++ GOTOYOURDREAM.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int choice;
+while (true){
+cout<<"system is running"<<endl;
+cout<<"press 1 to keep running, or 0 to exit:"<<endl;
+cin>>choice;
+if (choice == 0){break;} }
+cout<<"system stopped."<<endl;
+return 0;
+}
