ffb36cf55ae115db482a63da841136e86fd224ea
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-02-18 23:56:05 +0200

    new step

---
+++ star0.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+long long petabyt;
+cout<<"how much petabyt you need ?";
+cin>>petabyt;
+long long Subscriptions;
+cout<<"plan 1 (standar) petabyte = 2000000$";
+cout<<"plan 2 (vip) {Technical support (24/7) & Uptime Guarantee} petabyte = 3000000$ ";
+cin>>Subscriptions;
+double plan;
+cout<<"plan 1: singel Availability Zone (singel AZ)  ";
+cout<<"plan 2:Dual Availability Zones(Dual AZ) cost=total price * 1.7";
+cin>>plan;
+
+    
