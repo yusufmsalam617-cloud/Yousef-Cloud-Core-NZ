184ad0f0feb13ebca043682c4a591b9e04faaae3
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-03-01 02:23:13 +0200

    letsgooo

---
+++ letsgo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool secure(int n){    
+if(n>14 == false){cout<<"ALARM: Breach detected"<<endl;}
+return false;
+if(n<15 == true){cout<<"SYSTEM Secure"<<endl;}
+return true;
+}
+int main()
+{
+int n;    
+cout<<"enten Attempts numper"<<endl;
+cin>>n;
+secure(n);
+return 0;
+}
---
+++ letsgo3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+bool secure(int n){    
+if(n<15 == false){cout<<"ALARM: Breach detected"<<endl;}
+return false;
+if(n>15 == true){cout<<"SYSTEM Secure"<<endl;}
+return true;
+}
+int main()
+{
+while(true){    
+int n;    
+cout<<"enten Attempts numper"<<endl;
+cin>>n;
+secure(n);
+}
+return 0;
+}
