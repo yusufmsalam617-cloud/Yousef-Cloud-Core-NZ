de25de640485e735c1797041f4db21bc9ac46491
yusufmsalam617-cloud <yusufmsalam617@gmail.com>
2026-03-06 03:19:22 +0200

    new

--- kilobytes.cpp
+++ kilobytes.cpp
@@ -7,10 +7,10 @@ int kilobytes;
 cout<<"enter kilobytes num: "<<endl;
 cin>>kilobytes;
 int megabytes=kilobytes*1024;
-cout<<"your kilobytes is  " <<fixed <<setprecision <<megabytes<<"  megabytes  "<<endl;
+cout<<"your kilobytes is  " <<fixed <<setprecision(2) <<megabytes<<"  megabytes  "<<endl;
 int Gigabytes=megabytes*1024;
-cout<<"your kilobytes is  " <<fixed <<setprecision <<Gigabytes<<"  Gigabytes  "<<endl;
+cout<<"your kilobytes is  " <<fixed <<setprecision(2) <<Gigabytes<<"  Gigabytes  "<<endl;
 int Terabytes=Gigabytes*1024;
-cout<<"your kilobytes is  " <<fixed <<setprecision <<Terabytes<<"  Terabytes  "<<endl;
+cout<<"your kilobytes is  " <<fixed <<setprecision(2) <<Terabytes<<"  Terabytes  "<<endl;
 return 0;
 }
